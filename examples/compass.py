ee1c06965dd4596f4bfc0ae7f2574426ddd7e1bc
David Honess <dave@raspberrypi.org>
2015-04-12 00:01:57 +0000

    improve logic

--- examples/compass.py
+++ examples/compass.py
@@ -2,7 +2,12 @@
 import sys
 from astro_pi import AstroPi
 
-edge_loop = [0, 1, 2, 3, 4, 5, 6, 7, 15, 23, 31, 39, 47, 55, 63, 62, 61, 60, 59, 58, 57, 56, 48, 40, 32, 24, 16, 8]
+# To get good results with the magnetometer you must first calibrate it using
+# the program in RTIMULib/Linux/RTIMULibCal
+# The calibration program will produce the file RTIMULib.ini
+# Copy it into the same folder as your Python code
+
+led_loop = [4, 5, 6, 7, 15, 23, 31, 39, 47, 55, 63, 62, 61, 60, 59, 58, 57, 56, 48, 40, 32, 24, 16, 8, 0, 1, 2, 3]
 
 ap = AstroPi()
 ap.set_rotation(0)
@@ -11,18 +16,24 @@ ap.clear()
 prev_x = 0
 prev_y = 0
 
+led_degree_ratio = len(led_loop) / 360.0
+
+ON = [0, 0, 255]  # Blue
+OFF = [0, 0, 0]
+
 while True:
     dir = ap.get_compass()
-    edge_index = int((len(edge_loop) / 360.0) * dir)
-    offset = edge_loop[edge_index]
+    dir_inverted = 360 - dir  # So LED appears to follow North
+    led_index = int(led_degree_ratio * dir_inverted)
+    offset = led_loop[led_index]
 
-    y = offset // 8
-    x = offset % 8
+    y = offset // 8  # row
+    x = offset % 8  # column
 
     if x != prev_x or y != prev_y:
-        ap.set_pixel_xy(prev_x, prev_y, [0, 0, 0])
+        ap.set_pixel_xy(prev_x, prev_y, OFF)
 
-    ap.set_pixel_xy(x, y, [0, 0, 255])
+    ap.set_pixel_xy(x, y, ON)
 
     prev_x = x
     prev_y = y
