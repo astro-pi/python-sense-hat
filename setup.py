82ed427a943dd25052717fc165eada9052b2d910
Ben Nuttall <ben@bennuttall.com>
2015-05-25 12:22:56 +0100

    Version bump -> v1.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name="astro-pi",
-    version="1.1.0",
+    version="1.1.1",
     author="Dave Honess",
     author_email="dave@raspberrypi.org",
     description="Python API for Astro Pi (Sense HAT) for the Raspberry Pi",
