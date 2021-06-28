40c700762ece39b8310314f5e43c57c6b17b6c53
George Boukeas <boukeas@gmail.com>
2021-06-28 14:57:12 +0100

    Improve documentation. Move the `StatusFile` interface to the emulator.

--- sense_hat/colour.py
+++ sense_hat/colour.py
@@ -3,68 +3,137 @@ Python library for the TCS34725 Color Sensor
 Documentation (including datasheet): https://ams.com/tcs34725#tab/documents
 """
 
-import smbus
-import glob
-
-import io, os, sys, errno
-import mmap
-from struct import Struct
-from collections import namedtuple
-
 from time import sleep
 
 _error_str = "Failed to initialise TCS34725 colour sensor."
 
+
 class HardwareInterface:
+    """
+    `HardwareInterface` is the abstract class that sits between the
+    `ColourSensor` class (providing the TCS34725 sensor API) and the
+    actual hardware. Using this intermediate layer of abstraction, a
+    `ColourSensor` object interacts with the hardware without being
+    aware of how this interaction is implemented.
+
+    Different subclasses of the `HardwareInterface` class can provide
+    access to the hardware through e.g. I2C, `libiio` and its system
+    files or even a hardware emulator.
+    """
 
     GAIN_VALUES = (1, 4, 16, 60)
     CLOCK_STEP = 0.0024 # the clock step is 2.4ms
 
+    @staticmethod
+    def max_value(integration_cycles):
+        """
+        The maximum raw value for the RBGC channels depends on the number
+        of integration cycles.
+        """
+        return 2**16 if integration_cycles >= 64 else 1024*integration_cycles
+
     def get_enabled(self):
+        """
+        Return True if the sensor is enabled and False otherwise
+        """
         raise NotImplementedError
 
-    def set_enabled(self, value):
+    def set_enabled(self, status):
+        """
+        Enable or disable the sensor, depending on the boolean `status` flag
+        """
         raise NotImplementedError
 
     def get_gain(self):
+        """
+        Return the current value of the sensor gain.
+        See GAIN_VALUES for the set of possible values.
+        """
         raise NotImplementedError
 
-    def set_gain(self, value):
+    def set_gain(self, gain):
+        """
+        Set the value for the sensor `gain`.
+        See GAIN_VALUES for the set of possible values.
+        """
         raise NotImplementedError
 
     def get_integration_cycles(self):
+        """
+        Return the current number of integration_cycles (1-256).
+        It takes `integration_cycles` * CLOCK_STEP to obtain a new
+        sensor reading.
+        """
         raise NotImplementedError
 
-    def set_integration_cycles(self, value):
+    def set_integration_cycles(self, integration_cycles):
+        """
+        Set the current number of integration_cycles (1-256).
+        It takes `integration_cycles` * CLOCK_STEP to obtain a new
+        sensor reading.
+        """
         raise NotImplementedError
 
-    def get_all(self):
+    def get_raw(self):
+        """
+        Return a tuple containing the raw values of the RGBC channels.
+        The maximum for these raw values depends on the number of
+        integration cycles and can be computed using `max_value`.
+        """
         raise NotImplementedError
 
     def get_red(self):
+        """
+        Return a the raw value of the R (red) channel.
+        The maximum for this raw value depends on the number of
+        integration cycles and can be computed using `max_value`.
+        """
         raise NotImplementedError
 
     def get_green(self):
+        """
+        Return a the raw value of the G (green) channel.
+        The maximum for this raw value depends on the number of
+        integration cycles and can be computed using `max_value`.
+        """
         raise NotImplementedError
 
     def get_blue(self):
+        """
+        Return a the raw value of the B (blue) channel.
+        The maximum for this raw value depends on the number of
+        integration cycles and can be computed using `max_value`.
+        """
         raise NotImplementedError
 
     def get_clear(self):
+        """
+        Return a the raw value of the C (clear light) channel.
+        The maximum for this raw value depends on the number of
+        integration cycles and can be computed using `max_value`.
+        """
         raise NotImplementedError
 
+
+### An I2C implementation of the abstract colour sensor `HardwareInterface`
 
 def _raw_wrapper(register):
     """
     Returns a function that retrieves the sensor reading at `register`.
-    The CRGB readings are all retrieved from the sensor in an identical fashion.
-    This is a factory function that implements this retrieval method.
+    The RGBC readings are all retrieved from the sensor in an identical
+    fashion. This is a factory function that implements this retrieval method.
     """
-    def get_raw(self):
-        return self.read(register)
-    return get_raw
+    def get_raw_register(self):
+        return self._read(register)
+    return get_raw_register
 
 class I2C(HardwareInterface):
+    """
+    An implementation of the `HardwareInterface` for the TCS34725 sensor
+    that uses I2C to control the sensor and retrieve measurements.
+
+    Use the datasheet as a reference: https://ams.com/tcs34725#tab/documents
+    """
 
     # device-specific constants
     BUS = 1
@@ -95,10 +164,14 @@ class I2C(HardwareInterface):
     AVALID = 0x01
 
     GAIN_REG_VALUES = (0x00, 0x01, 0x02, 0x03)
-    GAIN_TO_REG = dict(zip(HardwareInterface.GAIN_VALUES, GAIN_REG_VALUES)) # maps gain values to register values
+    # map gain values to register values and vice-versa
+    GAIN_TO_REG = dict(zip(HardwareInterface.GAIN_VALUES, GAIN_REG_VALUES))
     REG_TO_GAIN = dict(zip(GAIN_REG_VALUES, HardwareInterface.GAIN_VALUES))
 
     def __init__(self):
+
+        import smbus
+        import glob
 
         try:
             self.bus = smbus.SMBus(self.BUS)
@@ -107,7 +180,7 @@ class I2C(HardwareInterface):
             raise RuntimeError(f'{_error_str}{explanation}') from e
         
         try:
-            id = self.read(self.ID)
+            id = self._read(self.ID)
         except Exception as e:
             explanation = " (sensor not present)"
             raise RuntimeError(f'{_error_str}{explanation}') from e
@@ -121,39 +194,79 @@ class I2C(HardwareInterface):
         """Returns True if I2C is enabled or False otherwise."""
         return next(glob.iglob('/sys/bus/i2c/devices/*'), None) is not None
 
-    def read(self, attribute):
+    def _read(self, attribute):
+        """
+        Read and return the value of a specific register (`attribute`) of the
+        TCS34725 colour sensor.
+        """
         return self.bus.read_byte_data(self.ADDR, attribute)
     
-    def write(self, attribute, value):
+    def _write(self, attribute, value):
+        """
+        Write a value in a specific register (`attribute`) of the
+        TCS34725 colour sensor.
+        """
         self.bus.write_byte_data(self.ADDR, attribute, value)
 
     def get_enabled(self):
-        return self.read(self.ENABLE) == (PON | AEN)
+        """
+        Return True if the sensor is enabled and False otherwise
+        """
+        return self._read(self.ENABLE) == (PON | AEN)
 
-    def set_enabled(self, value):
-        if value:
-            self.write(self.ENABLE, self.PON)
+    def set_enabled(self, status):
+        """
+        Enable or disable the sensor, depending on the boolean `status` flag
+        """
+        if status:
+            self._write(self.ENABLE, self.PON)
             sleep(self.CLOCK_STEP) # From datasheet: "there is a 2.4 ms warm-up delay if PON is enabled."
-            self.write(self.ENABLE, self.ON)
+            self._write(self.ENABLE, self.ON)
         else:
-            self.write(self.ENABLE, self.OFF)
+            self._write(self.ENABLE, self.OFF)
         sleep(self.CLOCK_STEP)
 
     def get_gain(self):
-        register_value = self.read(self.CONTROL)
+        """
+        Return the current value of the sensor gain.
+        See GAIN_VALUES for the set of possible values.
+        """
+        register_value = self._read(self.CONTROL)
+        # map the register value to an actual gain value
         return self.REG_TO_GAIN[register_value]
 
-    def set_gain(self, value):
-        register_value = self.GAIN_TO_REG[value]
-        self.write(self.CONTROL, register_value)
+    def set_gain(self, gain):
+        """
+        Set the value for the sensor `gain`.
+        See GAIN_VALUES for the set of possible values.
+        """
+        # map the specified value for `gain` to a register value
+        register_value = self.GAIN_TO_REG[gain]
+        self._write(self.CONTROL, register_value)
 
     def get_integration_cycles(self):
-        return 256 - self.read(self.ATIME)
+        """
+        Return the current number of integration_cycles (1-256).
+        It takes `integration_cycles` * CLOCK_STEP to obtain a new
+        sensor reading.
+        """
+        return 256 - self._read(self.ATIME)
 
-    def set_integration_cycles(self, value):
-        self.write(self.ATIME, 256-value)
+    def set_integration_cycles(self, integration_cycles):
+        """
+        Set the current number of integration_cycles (1-256).
+        It takes `integration_cycles` * CLOCK_STEP to obtain a new
+        sensor reading.
+        """
+        self._write(self.ATIME, 256-integration_cycles)
 
-    def get_all(self):
+    def get_raw(self):
+        """
+        Return a tuple containing the raw values of the RGBC channels.
+        The maximum for these raw values depends on the number of
+        integration cycles and can be computed using `max_value`.
+        """
+        # The 4-tuple is retrieved using a *single read*.
         block = self.bus.read_i2c_block_data(self.ADDR, self.CDATA, 8)
         return (
             (block[3] << 8) + block[2],
@@ -162,109 +275,18 @@ class I2C(HardwareInterface):
             (block[1] << 8) + block[0]
         )
 
+    """
+    The methods below return the raw value of the R, G, B or Clear channels.
+    The maximum for these raw value depends on the number of integration
+    cycles and can be computed using `max_value`.
+    Use these methods if you only make use of one channel reading per iteration.
+    Otherwise, you are probably better off using `get_raw`, to retrieve all
+    channels in a single read.
+    """
     get_red = _raw_wrapper(RDATA)
     get_green = _raw_wrapper(GDATA)
     get_blue = _raw_wrapper(BDATA)
     get_clear = _raw_wrapper(CDATA)
-
-
-class StatusFile(HardwareInterface):
-
-    COLOUR_DATA = Struct(str(
-        '@'   # native mode
-        '?'   # enabled
-        '4I'  # RGBC
-        'H'   # gain
-        'H'   # integration cycles
-        ))
-
-    ColourData = namedtuple(
-        'ColourData', (
-            'enabled', 'R', 'G', 'B', 'C', 'gain', 'integration_cycles'))
-
-    def __init__(self):
-        self.init_file()
-
-    @staticmethod
-    def filename():
-        """
-        Return the filename used to represent the state of the emulated sense HAT's
-        co sensor. On UNIX we try ``/dev/shm`` then fall back to ``/tmp``; on
-        Windows we use whatever ``%TEMP%`` contains
-        """
-        fname = 'rpi-sense-emu-colour'
-        if sys.platform.startswith('win'):
-            # just use a temporary file on Windows
-            return os.path.join(os.environ['TEMP'], fname)
-        else:
-            if os.path.exists('/dev/shm'):
-                return os.path.join('/dev/shm', fname)
-            else:
-                return os.path.join('/tmp', fname)
-
-    def init_file(self):
-        """
-        Opens the file representing the state of the humidity sensor. The
-        file-like object is returned.
-        If the file already exists we simply make sure it is the right size. If
-        the file does not already exist, it is created and zeroed.
-        """
-        try:
-            # Attempt to open the colour sensor's file and ensure it's the right size
-            fd = io.open(self.filename(), 'r+b', buffering=0)
-            fd.seek(self.COLOUR_DATA.size)
-            fd.truncate()
-        except IOError as e:
-            # If the colour sensor device file doesn't exist, zero it into existence
-            if e.errno == errno.ENOENT:
-                fd = io.open(self.filename(), 'w+b', buffering=0)
-                fd.write(b'\x00' * self.COLOUR_DATA.size)
-            else:
-                raise IOError from e
-        else:
-            self._fd = fd
-            self._map = mmap.mmap(self._fd.fileno(), 0, access=mmap.ACCESS_WRITE)
-
-    def read(self):
-        return self.ColourData(*self.COLOUR_DATA.unpack_from(self._map))
-
-    def write(self, value):
-        self.COLOUR_DATA.pack_into(self._map, 0, *value)
-
-    def get_enabled(self):
-        return self.read().enabled
-
-    def set_enabled(self, value):
-        self.write(self.read()._replace(enabled=value))
-
-    def get_gain(self):
-        return self.read().gain
-
-    def set_gain(self, value):
-        self.write(self.read()._replace(gain=value))
-
-    def get_integration_cycles(self):
-        return self.read().integration_cycles
-
-    def set_integration_cycles(self, value):
-        self.write(self.read()._replace(integration_cycles=value))
-
-    def get_all(self):
-        _, R, G, B, C, _, _ = self.read()
-        return (R, G, B, C)
-
-    def get_red(self):
-        print(f"DEBUG: {self.read()}")
-        return self.read().R
-
-    def get_green(self):
-        return self.read().G
-
-    def get_blue(self):
-        return self.read().B
-
-    def get_clear(self):
-        return self.read().C
 
 
 class ColourSensor:
@@ -288,26 +310,26 @@ class ColourSensor:
         return self.interface.get_gain()
 
     @gain.setter
-    def gain(self, value):
-        if value in self.interface.GAIN_VALUES:
-            self.interface.set_gain(value)
+    def gain(self, gain):
+        if gain in self.interface.GAIN_VALUES:
+            self.interface.set_gain(gain)
         else:
-            raise RuntimeError(f'Cannot set gain to {value}. Values: {self.interface.GAIN_VALUES}')
+            raise RuntimeError(f'Cannot set gain to {gain}. Values: {self.interface.GAIN_VALUES}')
 
     @property
     def integration_cycles(self):
         return self.interface.get_integration_cycles()
 
     @integration_cycles.setter
-    def integration_cycles(self, cycles):
-        if 1 <= cycles <= 256:
-            self.interface.set_integration_cycles(cycles)
-            self._integration_time = cycles * self.interface.CLOCK_STEP
-            self._max_value = 2**16 if cycles >= 64 else 1024*cycles
+    def integration_cycles(self, integration_cycles):
+        if 1 <= integration_cycles <= 256:
+            self.interface.set_integration_cycles(integration_cycles)
+            self._integration_time = integration_cycles * self.interface.CLOCK_STEP
+            self._max_value = self.interface.max_value(integration_cycles)
             self._scaling = self._max_value // 256
             sleep(self.interface.CLOCK_STEP)
         else:
-            raise RuntimeError(f'Cannot set integration cycles to {cycles} (1-256)')
+            raise RuntimeError(f'Cannot set integration cycles to {integration_cycles} (1-256)')
 
     @property
     def integration_time(self):
@@ -319,7 +341,7 @@ class ColourSensor:
 
     @property
     def colour_raw(self):
-        return self.interface.get_all()
+        return self.interface.get_raw()
 
     @property
     def colour(self):
